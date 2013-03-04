a183b44db5dc1d048ffb7e70148a5927bebd6667
Akshay <asingnurkar@aduxmedia.com>
2013-03-04 14:27:26 -0500

    Exclusion search implemented in View all Backlinks view

--- Controller/BacklinkController.php
+++ Controller/BacklinkController.php
@@ -17,48 +17,48 @@ use Shaythamc\LinkManagementBundle\Form\Type\BacklinkType;
 use Shaythamc\LinkManagementBundle\Entity\Transaction;
 
 class BacklinkController extends Controller{
-	
+
 	public function addBacklinkAction(Request $request){
-		
+
 		$backlink = new Backlink();
-		
+
 		$form = $this->createForm(new BacklinkType(), $backlink);
-		
+
 		if($request->isMethod('POST')){
 			$form->bind($request);
 			$backlinkManager = $this->get('lmt_backlink_manager');
 			$backlinkArray = get_object_vars($backlink);
-            $backlinkManager->addBacklink($backlinkArray);			
+            $backlinkManager->addBacklink($backlinkArray);
 		}
-		
+
 		return $this->render('ShaythamcLinkManagementBundle:Backlink:add.html.twig', array(
 			'form' => $form->createView(),
 		));
 	}
-	
+
 	public function viewAllBacklinksAction(Request $request){
-		
+
 		$backlinkManager = $this->get('lmt_backlink_manager');
 		$backlinks = $backlinkManager->getAllBacklinks();
-        
-        foreach($backlinks as $key => $blink){
-            
-            $siteUrl = $backlinkManager->getBacklinkSite($blink['siteId']);
-            
-            $backlinks[$key]['siteUrl'] = $siteUrl;
 
-        
+        foreach($backlinks as $key => $blink){
+
+            $site = $backlinkManager->getBacklinkSite($blink['siteId']);
+            $backlinks[$key]['siteUrl'] = $site['url'];
+            $backlinks[$key]['siteName'] = $site['title'];
+
+
 //            echo '<pre>';
 //            var_dump($backlinks);
 //            echo '</pre>';
-            
-        }
-		
 
-        
+        }
+
+
+
 		return $this->render('ShaythamcLinkManagementBundle:Backlink:all.html.twig', array(
 			'backlinks' => $backlinks,
 		));
-		
+
 	}
 }
